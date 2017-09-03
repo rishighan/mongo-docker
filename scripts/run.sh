db5a4f1a0a5da94fafe6a45db8d3daa961b23aaa
Rishi Ghan <rishi.ghan@yodle.com>
2017-09-02 20:09:31 -0400

    Script works with seed data

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # ref: mongodump --host localhost --db rishighan
-# run command like so ./run.sh </path/to/dump/>
+# run command like so ./run.sh </path/to/dump/> <mongo database>
 
-# if path/to/dump is not empty and contains .bson, do a mongorestore,
-# which creates databases, and re-creates indices
+# if path/to/dump is not empty and contains .bson,
+# do a mongorestore, which creates databases, and re-creates indices
+
+# args
+# 0 -> directory of db dump
+# 1 -> database name
 
 args=("$@")
-if [ -d ${args[0]} ] && [ -z "$(ls -A ${args[0]})" ]; then
-    files=(${args[0]}/*.bson)
-        echo "Directory \"${args[0]}\" is empty"
-    else
-        echo "BSON data found in ${args[0]}, proceeding"
-    fi
+if [ -d ${args[0]} ] ; then
+        if [ -z "$(ls -A ${args[0]})" ]; then
+            echo "Directory \"${args[0]}\" is empty"
+        else
+            echo -e "==> ${args[0]} is not empty, proceeding..."
+            count=`ls ${args[0]} -1 *.bson 2>/dev/null | wc -l`
+            if [ $count != 0 ]; then
+                echo "==> BSON files found..."
+                mongorestore --drop -d ${args[1]} ${args[0]}
+            fi
+        fi
+ fi
 
 
